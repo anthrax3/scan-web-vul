af1390774e97f4dd13e890a6d56b3cf0329693e6
Jean-Guy Billard <jean-guy.billard@keelut.com>
2017-11-30 22:03:18 -0500

    Add uniscan

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php 
 //git@github.com:jeanguybillard/scan-web-vul.git
+$optind = null;
+$opts = getopt('h:');
+
+exec("nikto -h ".escapeshellcmd($opts["h"])."");
+exec("uniscan -u ".escapeshellcmd($opts["h"])." -qweds"); 
 
